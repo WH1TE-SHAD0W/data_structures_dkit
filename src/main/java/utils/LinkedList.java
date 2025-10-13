025a804ff6070c6fbf6bb1e526478f29d3c5a0f4
D00271292 <d00271292@student.dkit.ie>
2025-10-13 16:30:30 +0100

    feat: Node based structure

---
+++ src/main/java/utils/LinkedList.java
@@ -0,0 +1,56 @@
+package utils;
+
+public class LinkedList {
+    private int size;
+    private Node head;
+    private Node tail;
+
+    public LinkedList() {
+        this.size = 0;
+    }
+
+    private static class Node {
+        private Node next;
+        private int data;
+
+        public Node(int data) {
+            this.next = null;
+            this.data = data;
+        }
+    }
+
+    public void add(int value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+        }
+        else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("");
+        }
+
+        Node newNode = new Node(value);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+        }
+
+        size++;
+    }
+
+}
