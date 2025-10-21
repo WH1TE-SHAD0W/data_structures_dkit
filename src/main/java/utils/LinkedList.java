292b359f1e50b8c3da4dd49b70100ff582a30c2c
Marek <marek@culak.sk>
2025-10-21 20:08:45 +0100

    feat: methods: deduplicate, join, rotate, display

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -32,6 +32,7 @@ public class LinkedList {
             }
             current.next = newNode;
         }
+        size++;
     }
 
     public void add(int index, int value) {
@@ -58,7 +59,7 @@ public class LinkedList {
     }
 
     public int get(int index) {
-        if (index < 0 || index > size) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("");
         }
 
@@ -68,6 +69,19 @@ public class LinkedList {
         }
 
         return current.data;
+    }
+
+    private Node get_node(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("");
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current;
     }
 
     public boolean isEmpty() {
@@ -103,9 +117,9 @@ public class LinkedList {
                     current = current.next;
                 }
             }
-            return false;
         }
-        throw new NullPointerException("Can't loop through an empty list!");
+        return false;
+//        throw new NullPointerException("Can't loop through an empty list!");
     }
 
     public int set(int index, int value) {
@@ -127,12 +141,85 @@ public class LinkedList {
         return old_value;
     }
 
-    // todo: size()
-    // todo: get()
-    // todo: isEmpty()
-    // todo: indexOf()
-    // todo: clear()
-    // todo: contains()
-    // todo: set()
+    public LinkedList deduplicate() {
+        LinkedList new_list = new LinkedList();
 
+        Node current = head;
+
+        for (int i = 0; i < this.size; i++) {
+            if (!new_list.contains(current.data)) {
+                new_list.add(current.data);
+            }
+            current = current.next;
+        }
+
+        return new_list;
+    }
+
+    public void join(LinkedList other) {
+        Node current = head;
+        for (int i = 0; i < this.size; i++) {
+            current = current.next;
+            if (current.next == null) {
+                current.next = other.head;
+            }
+        }
+        size += other.size;
+    }
+
+    public LinkedList rotate(int positions) {
+        int index = size - positions;
+
+        LinkedList new_list = new LinkedList();
+
+        Node current = this.get_node(index);
+        for (int i = 0; i < positions; i++) {
+            new_list.add(current.data);
+            current = current.next;
+        }
+        current = head;
+        for (int i = 0; i < index; i++) {
+            new_list.add(current.data);
+            current = current.next;
+        }
+
+        return new_list;
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(1);
+
+        LinkedList rotated = list.rotate(2);
+        list.display();
+
+        rotated = rotated.rotate(1);
+        rotated.display();
+
+        rotated = rotated.rotate(1);
+        rotated.display();
+
+        rotated = rotated.rotate(1);
+        rotated.display();
+
+    }
 }
+
+    // todo: deduplicate() -> should return a new list containing one instance of each value from current list
+    // todo: join() -> should take in a second list and add its contents onto this list
+    // todo: rotate() -> should take in the number of positions by which the list should be rotated. For example, if
+    //  1 is supplied, it should take the final element from the list and move it to the start of the list
+
